0b283af2685c8ab2d949eb78ecc14abe4f804e73
Elvis Pfützenreuter <epxx@epxx.co>
2019-06-23 21:55:10 -0300

    Change keycodes of numpad (independent of NumLock status)

--- keyboards/ymd96/keymaps/epx/keymap.c
+++ keyboards/ymd96/keymaps/epx/keymap.c
@@ -28,26 +28,28 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* Layer 0, default layer
-   *  | Esc  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 | || > |  <<  |  >>  | vol+ | vol- | Bkspc |
+   *  | Esc  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 | PrtSc |ScrLck|Pause|  vol- | vol+ | play |
    *  |  ~`  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |    0 |    - |    = |    BkSpc    |NumLck|   /  |   *  |   -   |
    *  |   Tab   |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |    P |    { |    } |     \    |  7   |   8  |   9  |       |
-   *  |   Ctrl    |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |    ; |   '  |     Return     |  4   |   5  |   6  |   +   |
+   *  |   Ctrl    |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |    ; |   '  |     Return    |  4   |   5  |   6  |   +   |
    *  |    LShft     |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |       RShft       |  1   |   2  |   3  |       | 
    *  | Ctrl  |  Alt  |  Win  |                  Space                      |  Win  |  Fn   |  Alt  |   Ctrl   |      0      |   .  | Enter |
    * 
    * Layout is inspired on Model F.
    * Extra keys on top right, as well as some seldom-used keys, are media keys in default layer.
    * Num Lock toogles layer, so the numeric block works as a Model F navigation block in a Mac.
-   * (Fn-Num Lock operates as conventional Num Lock in case the keyboard needs to be used with Windows or Linux.)
+   * Numpad generates KC_n instead of KC_Pn codes to guarantee they will be interpreted as
+   * intended, regardless of Num Lock status.
+   * (Fn-Num Lock operates as conventional Num Lock in case of need.)
    * Caps Lock operates normally but it takes FN to work, the key is Ctrl by default, like in Model F.
    */
   [_DEFLT] = KEYMAP(
-    KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,  KC_F12,  KC_MPLY, KC_MPRV, KC_MNXT, KC_VOLD, KC_VOLU, KC_BSPC, \
-    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS, KC_EQL,  KC_BSPC,          TO(_AR), KC_PSLS, KC_PAST, KC_PMNS, \
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC, KC_RBRC, KC_BSLS,          KC_P7,   KC_P8,   KC_P9,   KC_PPLS, \
-    KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT, KC_ENT,                    KC_P4,   KC_P5,   KC_P6,   KC_PPLS, \
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,                            KC_P1,   KC_P2,   KC_P3,   KC_PENT, \
-    KC_LCTL, KC_LALT,KC_LGUI,                     KC_SPC,                   KC_RGUI, KC_NO, MO(_RAISE), KC_RALT,  KC_RCTL,                  KC_P0,   KC_P0,   KC_PDOT  \
+    KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,  KC_F12,  KC_PSCR, KC_SLCK, KC_PAUSE, KC_VOLD, KC_VOLU, KC_MPLY, \
+    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS, KC_EQL,  KC_BSPC,          TO(_AR),  KC_PSLS, KC_PAST, KC_PMNS, \
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC, KC_RBRC, KC_BSLS,          KC_7,     KC_8,    KC_9,    KC_PPLS, \
+    KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT, KC_ENT,                    KC_4,     KC_5,    KC_6,    KC_PPLS, \
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,                            KC_1,     KC_2,    KC_3,    KC_PENT, \
+    KC_LCTL, KC_LALT,KC_LGUI,                     KC_SPC,                   KC_RGUI, KC_NO,   KC_RALT,  KC_RCTL, MO(_RAISE),                KC_0,     KC_0,    KC_DOT  \
   ),
 
   /* Layer 1, Num Lock activated 
@@ -68,7 +70,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   /* Layer 2, raise layer
-   *  |      |      |      |      |      |      |      |      |      |      |      |      |      | PrtSc |Pause|ScrLck| stop | mute |  Del  |
+   *  |      |      |      |      |      |      |      |      |      |      |      |      |      |      |      |      | prev | next | stop  |
    *  |          | RGB Toggle | RGB Hue - | RGB Hue + |   |   |   |   |   |   |    |      |      |     Del     |NumLck|      |      |       |
    *  |          | RGB Mode + | RGB Satu- | RGB Satu+  |   |   |   |   |   |   |      |      |      |          |      |      |      |       |
    *  | Caps Lck | RGB Mode - | RGB Brgt- | RGB Brgt+   |   |   |   |   |   |   |   |   |     |                |      |      |      |       |
@@ -76,7 +78,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    *  |       |       |       |                                             | Win Menu  |      |        |      |             |      |       |
    */ 
   [_RAISE] = KEYMAP(
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PSCR, KC_PAUS,  KC_SLCK, KC_MUTE, KC_MSTP, KC_DEL, \
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, KC_MPRV, KC_MNXT, KC_MSTP, \
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL,            KC_NLCK, _______, _______, _______, \
     _______, RGB_TOG, RGB_HUD, RGB_HUI, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______, _______, \
     KC_CAPS, RGB_MOD, RGB_SAD, RGB_SAI, _______, _______, _______, _______, _______, _______, _______,  _______, _______,                   _______, _______, _______, _______, \
