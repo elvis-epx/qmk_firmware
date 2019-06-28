f019a5c11ca1d38f77c23212f4ec1891e276092b
Elvis Pfutzenreuter <epxx@epxx.co>
2019-06-28 09:54:46 -0300

    Adotado layout JJ50 gerado pela ferramenta Web

--- keyboards/jj50/keymaps/epx/keymap.c
+++ keyboards/jj50/keymaps/epx/keymap.c
@@ -1,28 +1,7 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-
-    [0] = LAYOUT( \
-        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,   \
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,   \
-        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,    \
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,   \
-        MO(1),   MO(1),   KC_LALT, KC_LGUI, KC_SPC,  KC_SPC,  KC_SPC,  KC_SPC,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT    \
-    ),
-
-    [1] = LAYOUT( \
-       KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_DEL,   \
-       _______, KC_F11,  KC_F12,  KC_MINS, KC_MINS, KC_MINS, KC_MINS, KC_MINS, KC_MINS, KC_MINS, KC_MINS, KC_MINS,  \
-       KC_CAPS, KC_EQL,  KC_EQL,  KC_EQL,  KC_EQL,  KC_EQL,  KC_EQL,  KC_EQL,  KC_EQL,  KC_EQL,  KC_EQL,  KC_INS,   \
-       _______, KC_LBRC, KC_LBRC, KC_LBRC, KC_LBRC, KC_LBRC, KC_RBRC, KC_RBRC, KC_RBRC, KC_RBRC, KC_RBRC, _______,  \
-       MO(2),   MO(2),   _______, KC_APP,  _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END    \
-    ),
-
-    [2] = LAYOUT( \
-        xxxxxxx, xxxxxxx,  xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, KC_PAUS,  \
-        xxxxxxx, RGB_TOG,  RGB_HUD, RGB_HUI, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, KC_PSCR, xxxxxxx,  \
-        xxxxxxx, RGB_MOD,  RGB_SAD, RGB_SAI, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx,  \
-        xxxxxxx, RGB_RMOD, RGB_VAD, RGB_VAI, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx,  \
-        xxxxxxx, xxxxxxx,  xxxxxxx, xxxxxxx, KC_MPLY, KC_MPLY, KC_MPLY, KC_MPLY, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT  \
-    )
+	[0] = LAYOUT(KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSPC, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSLS, KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_ENT, KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, MO(1), MO(1), KC_LALT, KC_LGUI, KC_SPC, KC_SPC, KC_SPC, KC_SPC, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT),
+	[1] = LAYOUT(KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_DEL, KC_INS, KC_F11, KC_F12, KC_MINS, KC_MINS, KC_MINS, KC_MINS, KC_MINS, KC_MINS, KC_MINS, KC_MINS, KC_QUOT, KC_CAPS, KC_EQL, KC_EQL, KC_EQL, KC_EQL, KC_EQL, KC_EQL, KC_EQL, KC_EQL, KC_EQL, KC_EQL, KC_QUOT, KC_TRNS, KC_LBRC, KC_LBRC, KC_LBRC, KC_LBRC, KC_LBRC, KC_RBRC, KC_RBRC, KC_RBRC, KC_RBRC, KC_RBRC, KC_TRNS, MO(2), MO(2), KC_TRNS, KC_APP, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_HOME, KC_PGDN, KC_PGUP, KC_END),
+	[2] = LAYOUT(KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PSCR, KC_SLCK, KC_PAUS, KC_TRNS, RGB_TOG, RGB_HUD, RGB_HUI, RGB_SPD, RGB_SPI, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PSCR, KC_TRNS, KC_TRNS, RGB_MOD, RGB_SAD, RGB_SAI, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RGB_RMOD, RGB_VAD, RGB_VAI, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MPLY, KC_MPLY, KC_MPLY, KC_MPLY, KC_MPRV, KC__VOLDOWN, KC__VOLUP, KC_MNXT)
 };
