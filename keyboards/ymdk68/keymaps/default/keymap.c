1b4d77e51e9ca8e9113692d01e4e11419333dbca
Elvis Pfützenreuter <epxx@epxx.co>
2019-08-07 18:35:18 -0300

    Change in YMDK68

--- keyboards/ymdk68/keymaps/default/keymap.c
+++ keyboards/ymdk68/keymaps/default/keymap.c
@@ -22,15 +22,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,          KC_DEL,
         KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,                    KC_PGUP,
         KC_LSFT, KC_NO,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   KC_PGDN,
-        KC_RCTL, KC_LALT, KC_LGUI,                                 KC_SPC,                        KC_VOLD, KC_VOLU, MO(1),   KC_LEFT, KC_DOWN, KC_RGHT
+        KC_RCTL, KC_LALT, KC_LGUI,                                 KC_SPC,                        KC_RGUI, KC_RALT, MO(1),   KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
     LAYOUT_all(
         MO(2),   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PSCR, KC_SLCK, KC_PAUSE,_______,          KC_INS,
-        KC_CAPS, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                   KC_HOME,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          KC_MPLY, KC_END,
-        _______, _______, KC_APP,                                 _______,                        KC_MUTE, KC_MUTE, _______, KC_MPRV, KC_MSTP, KC_MNXT 
+        KC_CAPS, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MPLY,                   KC_HOME,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MSTP,          KC_VOLU, KC_END,
+        _______, _______, _______,                                 _______,                        KC_APP, _______, _______, KC_MPRV, KC_VOLD, KC_MNXT 
     ),
 
     LAYOUT_all(
