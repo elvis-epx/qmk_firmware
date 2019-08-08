2841a11126b28980296861570de836ce455f425c
Elvis Pfützenreuter <epxx@epxx.co>
2019-08-08 01:44:40 -0300

    Improved layout amj40

--- keyboards/amj40/keymaps/epx/keymap.c
+++ keyboards/amj40/keymaps/epx/keymap.c
@@ -1,37 +1,29 @@
 #include QMK_KEYBOARD_H
 
-#define _QWERTY 0
-#define _LOWER 1
-#define _RAISE 2
-
-enum custom_keycodes {
-  QWERTY = SAFE_RANGE,
-  LOWER,
-  RAISE
-};
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_QWERTY] = LAYOUT( \
-        KC_ESC,  KC_Q,   KC_W,   KC_E,    KC_R,   KC_T,   KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,   KC_BSPC, \
-        KC_TAB,  KC_A,   KC_S,   KC_D,    KC_F,   KC_G,   KC_H,   KC_J,    KC_K,    KC_L,    KC_ENT,\
-        KC_LSFT, KC_Z,   KC_X,   KC_C,    KC_V,   KC_B,   KC_N,   KC_M,    KC_SLSH, KC_UP,   KC_RSFT, \
-        KC_LCTL, KC_LALT, KC_LGUI, LT(_LOWER, KC_SPC), LT(_RAISE, KC_SPC), KC_LEFT, KC_DOWN, KC_RIGHT \
-		),
+    [0] = LAYOUT( \
+        KC_ESC,  KC_Q,    KC_W,   KC_E,     KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,     KC_O,    KC_P,    KC_BSPC, \
+        KC_TAB,  KC_A,    KC_S,   KC_D,     KC_F,    KC_G,    KC_H,    KC_J,    KC_K,     KC_L,    KC_ENT,\
+        KC_LSFT, KC_Z,    KC_X,   KC_C,     KC_V,    KC_B,    KC_N,    KC_M,    KC_SLSH,  KC_UP,   KC_RSFT, \
+        KC_LCTL, KC_LALT, KC_LGUI, LT(1, KC_SPC), LT(1, KC_SPC),                KC_LEFT,  KC_DOWN, KC_RIGHT ),
 
-    [_LOWER] = LAYOUT( \
-        KC_GRV,  KC_F1,  KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,    KC_F8,   KC_F9,   KC_F10, KC_DEL, \
-        KC_CAPS, KC_F11, KC_F12,  _______, _______, _______, KC_SCLN, KC_QUOT,  KC_LBRC, KC_RBRC, KC_BSLS, \
-        _______, KC_APP, _______, _______, _______, _______, _______, KC_COMMA, KC_DOT,  KC_PGUP, _______, \
-        _______, _______, _______, LT(_LOWER, KC_SPC), LT(_RAISE, KC_SPC),     KC_HOME, KC_PGDN, KC_END \
-		),
+    [1] = LAYOUT( \
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,     KC_9,     KC_0,   KC_DEL, \
+        KC_CAPS, _______, _______, _______, KC_MINS, KC_EQL,  KC_SCLN, KC_QUOT, KC_LBRC,  KC_RBRC,  KC_BSLS, \
+        _______, KC_APP,  KC_PSCR, KC_SLCK, KC_PAUS, _______, KC_COMMA,KC_DOT,  _______,  KC_PGUP,  _______, \
+        _______, _______, _______,                MO(2),  MO(2),                KC_HOME,  KC_PGDN,  KC_END ),
+
+    [2] = LAYOUT( \
+        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,    KC_F9,    KC_F10,  KC_INS, \
+        _______, KC_F11,  KC_F12,  MO(3),   _______, _______, _______, _______, _______,  KC_MSTP,  KC_MPLY, \
+        _______, _______, _______, _______, _______, _______, _______, _______, KC_MUTE,  KC_VOLU,  _______, \
+        _______, _______, _______,           KC_NO,    KC_NO,                   KC_MPRV,  KC_VOLD,  KC_MNXT ),
 	
-    [_RAISE] = LAYOUT( \
-        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,     KC_9,     KC_0,   KC_INS, \
-        KC_CAPS, KC_MINS, KC_EQL,  _______, _______, _______, _______, KC_PSCR, KC_SLCK,  KC_PAUSE, KC_MPLY, \
-        _______, _______, _______, _______, _______, _______, _______, KC_COMMA,KC_DOT,   KC_VOLU,  KC_MUTE, \
-        _______, _______, _______, LT(_LOWER, KC_SPC), LT(_RAISE, KC_SPC),      KC_MPRV,  KC_VOLD,  KC_MNXT \
-		),
-
+    [3] = LAYOUT( \
+        _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______,  _______, \
+        _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______, \
+        _______, _______, _______, _______, _______, RESET,   _______, _______, _______,  _______,  _______, \
+        _______, _______, _______,     _______,       _______,                  _______,  _______,  _______ ),
 };
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
@@ -42,7 +34,7 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
- 
+/*
   switch (keycode) {
     
     case LOWER:
@@ -62,5 +54,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       return false;
       break;
   }
+  */
   return true;
 }
