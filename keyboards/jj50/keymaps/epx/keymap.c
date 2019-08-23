850e2daf3fee2d0ef7d9e41ce088bdae653a1497
Elvis Pfützenreuter <epxx@epxx.co>
2019-08-23 02:27:37 -0300

    JJ50: symbol keys displace arrows

--- keyboards/jj50/keymaps/epx/keymap.c
+++ keyboards/jj50/keymaps/epx/keymap.c
@@ -6,33 +6,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[0] = LAYOUT(
 		KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,          KC_5,   KC_6,   KC_7,          KC_8,    KC_9,    KC_0,    KC_BSPC,
 		KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,          KC_T,   KC_Y,   KC_U,          KC_I,    KC_O,    KC_P,    KC_QUOT,
-		KC_MINS, KC_A,    KC_S,    KC_D,    KC_F,          KC_G,   KC_H,   KC_J,          KC_K,    KC_L,    KC_SCLN, KC_ENT,
+		KC_EQL,  KC_A,    KC_S,    KC_D,    KC_F,          KC_G,   KC_H,   KC_J,          KC_K,    KC_L,    KC_SCLN, KC_ENT,
 		KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,          KC_B,   KC_N,   KC_M,          KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-		KC_LCTL, KC_LALT, KC_LALT, KC_LGUI, LT(2, KC_SPC), KC_SPC, KC_SPC, LT(2, KC_SPC), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+		KC_LCTL, KC_LALT, KC_LALT, KC_LGUI, LT(2, KC_SPC), KC_SPC, KC_SPC, LT(2, KC_SPC), KC_MINS, KC_LBRC, KC_RBRC, KC_BSLS
 		),
 
 	[1] = LAYOUT(
-		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-		KC_LCTL, KC_LALT, KC_LALT, KC_RCTL, LT(2, KC_SPC), KC_SPC, KC_SPC, LT(2, KC_SPC), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+		_______, _______, _______, _______, _______,       _______, _______, _______,       _______, _______, _______, _______,
+		_______, _______, _______, _______, _______,       _______, _______, _______,       _______, _______, _______, _______,
+		_______, _______, _______, _______, _______,       _______, _______, _______,       _______, _______, _______, _______,
+		_______, _______, _______, _______, _______,       _______, _______, _______,       _______, _______, _______, _______,
+		KC_LCTL, KC_LALT, KC_LALT, KC_RCTL, LT(2, KC_SPC), KC_SPC,  KC_SPC,  LT(2, KC_SPC), _______, _______, _______, _______
 		),
 
 	[2] = LAYOUT(
 		KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_DEL,
-		_______, KC_F11,  KC_F12,  _______, _______, _______, KC_EQL,  KC_EQL,  KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
+		_______, KC_F11,  KC_F12,  _______, _______, _______, _______, _______, _______, _______, _______, _______,
 		KC_CAPS, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, 
 		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, 
-		_______, _______, KC_LGUI, KC_APP,  MO(3),   _______, _______, MO(3),   KC_HOME, KC_PGDN, KC_PGUP, KC_END
+		_______, _______, KC_LGUI, KC_APP,  MO(3),   _______, _______, MO(3),   KC_DOWN, KC_UP,   KC_LEFT, KC_RGHT
 		),
 
 	[3] = LAYOUT(
 		MO(4),   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_INS,
 		_______, RGB_TOG, RGB_HUD, RGB_HUI, RGB_SPD, RGB_SPI, _______, _______, KC_PSCR, KC_SLCK, KC_PAUS, _______,
 		_______, RGB_MOD, RGB_SAD, RGB_SAI, _______, _______, _______, _______, _______, TO(1),   _______, _______,
-		_______, RGB_RMOD,RGB_VAD, RGB_VAI, _______, _______, _______, TO(0),   KC_MPLY, KC_MUTE, KC_MUTE, KC_MPLY,
-		_______, _______, _______, _______, _______, _______, _______, _______, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT
+		_______, RGB_RMOD,RGB_VAD, RGB_VAI, _______, _______, _______, TO(0),   _______, _______, _______, _______,
+		_______, _______, _______, _______, _______, _______, _______, _______, KC_PGDN, KC_PGUP, KC_HOME, KC_END
 		),
 
 	[4] = LAYOUT(
