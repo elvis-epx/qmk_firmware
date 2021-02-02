775519540b4447aaadfb1eebf161b78c9d8f93f9
Elvis Pfützenreuter <epxx@epxx.co>
2021-02-02 01:14:39 -0300

    Duplo ALT

--- keyboards/wheatfield/split75/keymaps/epx/keymap.c
+++ keyboards/wheatfield/split75/keymaps/epx/keymap.c
@@ -9,7 +9,7 @@ KC_MUTE,  KC_MUTE,   KC_GRAVE, KC_1,    KC_2,    KC_3,   KC_4,   KC_5,   KC_6,  
 KC_MSTP,  KC_MSTP,   KC_TAB,   KC_Q,    KC_W,    KC_E,   KC_R,   KC_T,                 KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_LBRC,  KC_RBRC,  KC_BSLS,           KC_END,
 KC_MPLY,  KC_MPLY,   KC_LCTL,  KC_A,    KC_S,    KC_D,   KC_F,   KC_G,                 KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT,  KC_ENTER,                    KC_PGUP,
 KC_MPRV,  KC_MNXT,   KC_LSFT,  KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,                 KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH,                     KC_RSFT,  KC_UP,   KC_PGDN,
-KC_VOLD,  KC_VOLU,   KC_RCTL,  KC_LGUI, KC_LALT, KC_LGUI,KC_SPC,                       KC_SPC,         KC_RGUI, KC_RALT, MO(1),                       KC_LEFT,  KC_DOWN, KC_RIGHT
+KC_VOLD,  KC_VOLU,   KC_RCTL,  KC_LALT, KC_LALT, KC_LGUI,KC_SPC,                       KC_SPC,         KC_RGUI, KC_RALT, MO(1),                       KC_LEFT,  KC_DOWN, KC_RIGHT
 ),
   [1] = LAYOUT(
                      MO(2),    _______, _______, _______,_______,_______,_______,              _______,_______, _______, _______, _______,  _______,  KC_NLCK, KC_SLCK,  KC_INS,
