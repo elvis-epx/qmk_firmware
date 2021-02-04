d0536b22b1359f6f96d45c8490f2a513060c2aa7
Elvis Pfützenreuter <epxx@epxx.co>
2021-02-04 00:56:58 -0300

    Initial new Melody96 layout

--- keyboards/melody96/keymaps/epx/keymap.c
+++ keyboards/melody96/keymaps/epx/keymap.c
@@ -1,25 +1,60 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+  /*
+   * Layout is inspired on Model F.
+   * Num Lock A toogles layer, so the numeric block works as a Model F navigation block even in a Mac.
+   * Num Lock B works as a regular Num Lock, which is ignored by Mac but honored on PC.
+   * Caps Lock operates normally but it takes FN to work, the key is Ctrl by default, like in Model F.
+   * Seldom-used PC keys (Print Screen block) are media keys by default.
+   */
 	LAYOUT_hotswap(
-    KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PGDN, KC_PGUP, KC_HOME, KC_END,  KC_PSCR, KC_PAUSE,
-    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,          KC_DEL,  KC_PSLS, KC_PAST, KC_PMNS,
+    KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_VOLD, KC_VOLU, KC_MPRV, KC_MPLY, KC_MNXT, MO(2),
+    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,          TO(1),   KC_NLCK, KC_PAST, KC_PMNS,
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,          KC_P7,   KC_P8,   KC_P9,
     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,                    KC_P4,   KC_P5,   KC_P6,   KC_PPLS,
     KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   KC_P1,   KC_P2,   KC_P3,
-    KC_RCTL, KC_LALT, KC_LGUI,                            KC_SPC,                             KC_RALT, MO(1),   MO(1),   KC_LEFT, KC_DOWN, KC_RGHT, KC_P0,   KC_PDOT, KC_PENT),
+    KC_LCTL, KC_LALT, KC_LGUI,                            KC_SPC,                             KC_RALT, KC_RCTL, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT, KC_P0,   KC_PDOT, KC_PENT),
+
+  /* Num Lock activated 
+   *  ...
+   *  ... 
+   *  ... | Home |  Up  | Pg Up |       |
+   *  ... | Left | (no) | Right |   +   |
+   *  ... | End  | Down | Pg Dn |       |
+   *  ...        |  Ins |  Del  | Enter |
+   */
+	LAYOUT_hotswap(
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          TO(0),   _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          KC_HOME, KC_UP,   KC_PGUP,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                   KC_LEFT, KC_NO,   KC_RGHT, _______,
+    _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, KC_END , KC_DOWN, KC_PGDN, 
+    _______, _______, _______,                            _______,                            _______, _______, _______, _______, _______, _______, KC_INS,  KC_DEL, _______),
+
+   /*
+    * Fn activated: gives access to RGB control keys and Caps Lock
+    * PC keys (Print Screen, Scroll Lock and Pause)
+    * Arrow keys become Page Up, etc. just like a 60% keyboard
+    * Num Lock B becomes numpad slash since it is in the same spot
+    */
 
 	LAYOUT_hotswap(
-    MO(2),   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_NLCK, KC_SLCK,
-    _______, BL_TOGG, BL_DEC,  BL_INC,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          KC_INS,  _______, _______, _______,
+    MO(3),   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MSTP, KC_MUTE, KC_MUTE, KC_PSCR, KC_SLCK, KC_PAUS, _______,
+    _______, BL_TOGG, BL_DEC,  BL_INC,  _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL,           _______, KC_PSLS, _______, _______,
     _______, RGB_TOG, RGB_HUD, RGB_HUI, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______,
     KC_CAPS, RGB_MOD, RGB_SAD, RGB_SAI, _______, _______, _______, _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______,
-    _______,          RGB_RMOD,RGB_VAD, RGB_VAI, _______, _______, _______, _______, _______, _______, _______, _______,          KC_VOLU, _______, _______, _______, 
-    _______, _______, _______,                            KC_MPLY,                            KC_APP,  _______, _______, KC_MPRV, KC_VOLD, KC_MNXT, KC_MUTE, KC_MSTP, _______),
+    _______,          RGB_RMOD,RGB_VAD, RGB_VAI, _______, _______, _______, _______, _______, _______, _______, _______,          KC_PGUP, _______, _______, _______, 
+    _______, _______, KC_RGUI,                            _______,                            KC_APP,  _______, _______, KC_HOME, KC_PGDN, KC_END,  _______, _______, _______),
+
+   /* 
+    * Reset layer: Fn-ESC-Backspace or FN-ESC_Del puts keyboard in flash mode
+    */
 
 	LAYOUT_hotswap(
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          RESET,   _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RESET,            _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______,
     _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______,
